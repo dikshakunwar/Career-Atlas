d200a62bc59ea3aa5344b30f3c9edb07c486b92b
Diksha Kunwar <dikshakunwar02@gmail.com>
2026-07-12 22:19:15 +0530

    UI improvements done

--- frontend/src/pages/JobDetails.jsx
+++ frontend/src/pages/JobDetails.jsx
@@ -87,7 +87,9 @@ function JobDetails() {
           <div className="bg-white border border-gray-200 rounded-xl p-5">
             <h2 className="text-base font-semibold mb-3">Description</h2>
 
-            <p className="text-sm text-gray-600 leading-7">{job.description}</p>
+            <p className="font-normal text-sm whitespace-pre-line">
+              {job.description}
+            </p>
           </div>
 
           <div className="bg-white border border-gray-200 rounded-xl p-5 mt-5">
